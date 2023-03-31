636968d0c17e5a781739a3332e90def5a305f8ff
William Gao <williamgao520@gmail.com>
2023-03-30 18:17:59 -0700

    chore: pull in eslint

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ This is a minimalist starter kit for Babylon.js using Vite instead of webpack.
 To start the dev server, run: `yarn dev`.
 
 The sample scene from this guide is included: https://doc.babylonjs.com/guidedLearning/createAGame/gettingSetUp
+
+
+## Commands
+
+### `yarn dev`
+
+Start the local development server with HMR.
+
+### `yarn build`
+
+Generate a production build to directory specified by `--outDir`.
+
+### `yarn preview`
+
+Start a server to preview the static files of the production build.
+
+### `yarn lint`
+
+Lint files. Specify `--fix` to perform an inplace fix.
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,55 @@
-import "./style.css";
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core";
-
+import "./style.css"
+import "@babylonjs/core/Debug/debugLayer"
+import "@babylonjs/inspector"
+import {
+    Engine,
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+} from "@babylonjs/core"
 
 class App {
     constructor() {
-        let canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.getElementById("app")!.appendChild(canvas);
+        let canvas = document.createElement("canvas")
+        canvas.style.width = "100%"
+        canvas.style.height = "100%"
+        canvas.id = "gameCanvas"
+        document.getElementById("app")!.appendChild(canvas)
 
         // initialize babylon scene and engine
-        let engine = new Engine(canvas, true);
-        let scene = new Scene(engine);
+        let engine = new Engine(canvas, true)
+        let scene = new Scene(engine)
 
-        let camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        let light = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        let sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        let camera = new ArcRotateCamera(
+            "Camera",
+            Math.PI / 2,
+            Math.PI / 2,
+            2,
+            Vector3.Zero(),
+            scene
+        )
+        camera.attachControl(canvas, true)
+
+        new HemisphericLight("light1", new Vector3(1, 1, 0), scene)
+        MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene)
 
         // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            if (ev.key === 'i') {
+        window.addEventListener("keydown", ev => {
+            if (ev.key === "i") {
                 if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                    scene.debugLayer.hide()
                 } else {
-                    scene.debugLayer.show();
+                    scene.debugLayer.show()
                 }
             }
-        });
+        })
 
         engine.runRenderLoop(() => {
-            scene.render();
-        });
+            scene.render()
+        })
     }
 }
 
-new App();
+new App()
